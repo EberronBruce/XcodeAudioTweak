7698627cad855b48a58870f87bf12412d63ee2a4
Bruce Burgess <bruceburgess67@yahoo.com>
2016-03-02 12:53:04 +0700

    Add audio player

--- VoiceTweak/ViewController.swift
+++ VoiceTweak/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import AVFoundation //import Audio Video Foundation
 
 class ViewController: UIViewController {
-
+    
+    var audioPlayer : AVAudioPlayer? //Create an audio property
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //When the play button is pressed
+    @IBAction func playTapped(sender: UIButton) {
+        do{
+            //Set up a path to the audio file
+            let path = NSBundle.mainBundle().pathForResource("ExampleAudio", ofType: "m4a")
+            //set up url
+            let url = NSURL(fileURLWithPath: path!)
+            //try to play the sound
+            try self.audioPlayer = AVAudioPlayer(contentsOfURL: url)
+        } catch {
+            
+        }
     }
-
-
+    
+    
 }
 
